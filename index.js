17f96e4ec914734236eb943cca057057434c38a6
webzyro <gmbworkswebzyro@gmail.com>
2026-01-24 11:55:04 +0530

    create testimonial section

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ AOS.init({
     duration: 1000,
     once: true,
 });
+
+
+// Testimonial Carousel
+$(document).ready(function(){
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1000,
+        startPosition: 0, 
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10
+            },
+            768: {
+                items: 2,
+                margin: 20
+            },
+            992: {
+                items: 3,
+                margin: 30
+            }
+        }
+    });
+});
